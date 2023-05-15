210f1df3dd7f69a8ce430157b77986a157ed61e2
Paul Frank <pail@gmx.net>
2023-05-15 08:52:44 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,181 @@
+from threading import Lock
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_socketio import SocketIO, emit
+from flask_sqlalchemy import SQLAlchemy
+from flask_apscheduler import APScheduler
+
+import yaml
+import json
+import paho.mqtt.client as mqtt
+from devices import EvccDevice, Home
+
+import logging
+
+
+# instantiate the app
+db = SQLAlchemy()
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///energyassistant.db"
+
+db.init_app(app)
+socketio = SocketIO(app, async_mode=None, cors_allowed_origins="*")
+
+
+# initialize scheduler
+scheduler = APScheduler()
+
+
+
+class HomeMeasurement(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    solar_production = db.Column(db.Float)
+    solar_self_consumption = db.Column(db.Float)
+    home_consumption = db.Column(db.Float)
+    self_sufficiency = db.Column(db.Float)
+
+    
+class MeasurementDevice(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    deviceId = db.Column(db.String, nullable=False)
+    value = db.Column(db.Float)
+   
+
+# cron examples
+@scheduler.task("cron", id="do_job_2", minute="*")
+def job2():
+    """Sample job 2."""
+    print("Job 2 executed")
+
+
+# interval examples
+@scheduler.task("interval", id="do_job_1", seconds=30, misfire_grace_time=900)
+def job1():
+    """Sample job 1."""
+    print("Job 1 executed")  
+
+
+
+energyassistant_topic = "energyassistant"
+home = None
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+
+
+@socketio.event
+def connect():
+    emit('connection', {'data': '{"Connected": true}', 'count': 0})
+
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+# devices route
+@app.route('/api/devices', methods=['GET'])
+def get_devices():
+    devices_message = []
+    for device in home.devices:
+        devices_message.append(
+            {"name": device.name, "state": device.state, "icon": "mdi:mdi-car-electric"})
+    return jsonify(devices_message)
+
+
+def get_home_message():
+    devices_message = []
+    for device in home.devices:
+        devices_message.append(
+            {"name": device.name, "state": device.state, "icon": "mdi:mdi-car-electric"})    
+    home_message = {
+        "name": home.name,
+        "solar_production": home.solar_production,
+        "grid_supply": home.grid_supply,
+        "solar_self_consumption": home.solar_self_consumption,
+        "home_consumption": home.home_consumption,
+        "self_sufficiency": round(home.self_sufficiency * 100),
+        "solar_energy": home.solar_energy,
+        "consumed_energy": home.consumed_energy,
+        "self_sufficiency_today": round(home.solar_energy / home.consumed_energy * 100) if home.consumed_energy > 0 else 0.0,
+        "devices" : devices_message
+    }
+    return json.dumps(home_message)
+
+@app.route('/api/home', methods=['GET'])
+def get_home():
+    return get_home_message()
+
+
+def on_message(client, userdata, message):
+    # print("message received ", str(message.payload.decode("utf-8")))
+    # print("message topic=", message.topic)
+    # print("message qos=", message.qos)
+    # print("message retain flag=", message.retain)
+    home.update_state(message.topic, str(message.payload.decode("utf-8")))
+    socketio.emit('refresh',
+                  {'data': get_home_message()})
+
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print('Unexpected disconnection from MQTT, trying to reconnect')
+
+
+def on_connect(client, userdata, flags, rc):
+    print("mqtt connected")
+    client.publish("{energyassistant_topic}/status", "online")
+    for topic in home.mqtt_topics():
+        client.subscribe(topic + "/#", 0)
+
+
+def initialize():
+    print("Hello from Energy Assistant")
+    with app.app_context():
+        db.create_all()    
+    # if you don't wanna use a config, you can set options here:
+    scheduler.api_enabled = True
+    scheduler.init_app(app)
+    scheduler.start()    
+    with open("config.yaml", "r") as stream:
+        try:
+            config = yaml.safe_load(stream)
+            print(config)
+        except yaml.YAMLError as exc:
+            print(exc)
+        else:
+            global home
+            home_config = config.get("home")
+            if home_config is not None:
+                home = Home(home_config.get("name"), "homeassistant/sensor/solaredge_i1_ac_power",
+                "homeassistant/sensor/solaredge_m1_ac_power")
+                home.add_device(EvccDevice(
+                    "Keba", "evcc/loadpoints/1/chargePower"))
+                mqtt_config = config.get("mqtt")
+                if mqtt_config is not None:
+                    homeassistant_host = mqtt_config.get("host")
+
+                    topic = mqtt_config.get("topic")
+                    global energyassistant_topic
+                    if topic is not None:
+                        energyassistant_topic = topic
+
+                    mqttc = mqtt.Client("energy_assistant", 1883, 45)
+                    mqttc.username_pw_set(mqtt_config.get(
+                        "username"), mqtt_config.get("password"))
+                    mqttc.will_set(f"{energyassistant_topic}/status",
+                                payload="offline", qos=0, retain=True)
+                    mqttc.connect(homeassistant_host)
+                    mqttc.on_message = on_message
+                    mqttc.on_connect = on_connect
+                    mqttc.on_disconnect = on_disconnect
+                    mqttc.loop_start()
+
+
+initialize()
+
+if __name__ == '__main__':
+    socketio.run(app)
---
+++ devices/__init__.py
@@ -0,0 +1,125 @@
+"""The Device classes."""
+from datetime import datetime
+
+class Integrator:
+    def __init__(self):
+        self.last_measurement = None
+        self.last_timestamp = None
+        self._value = None
+
+
+    @property
+    def value(self):
+        return self._value
+    
+    def add_measurement(self, measurement: float, timestamp: float):
+        if self.last_measurement is None:
+            self.last_measurement = measurement
+            self.last_timestamp = timestamp
+            self._value = 0
+        else:
+            delta_t = timestamp - self.last_timestamp
+            self.last_timestamp = timestamp
+            print("Delta t: "+ str(delta_t))
+            if delta_t > 0.1:
+                if measurement > self.last_measurement:
+                    self._value = self._value + (delta_t * (self.last_measurement + (measurement - self.last_measurement) / 2))
+                else:
+                    self._value = self._value + (delta_t * (measurement + (self.last_measurement - measurement) / 2))
+
+
+class HomeassitantDevice:
+    def __init__(self, name, mqtt_topic):
+        self.name = name
+        self.mqtt_topic = mqtt_topic
+        self.state = None
+
+
+    def update_state(self, topic: str, message: str):
+        if topic == self.mqtt_topic + "/state":
+            self.state = float(message)
+
+class EvccDevice:
+    def __init__(self, name, mqtt_topic):
+        self.name = name
+        self.mqtt_topic = mqtt_topic
+        self.state = None
+
+    def update_state(self, topic: str, message: str):
+        if topic == self.mqtt_topic:
+            self.state = float(message)
+
+
+class Home:
+    def __init__(self, name, solar_mqtt_topic, grid_supply_mqtt_topic):
+        self.name = name
+        self.solar_mqtt_topic = solar_mqtt_topic
+        self.grid_supply_mqtt_topic = grid_supply_mqtt_topic
+        self.solar_production = 0
+        self.grid_supply = 0
+        self.devices = []
+        self._solar_energy = Integrator()
+        self._consumed_energy = Integrator()        
+
+    def add_device(self, device):
+        self.devices.append(device)
+        
+    @property
+    def home_consumption(self):
+        result = self.solar_production - self.grid_supply
+        if result > 0:
+            return result
+        else:
+            return 0
+
+    @property
+    def solar_self_consumption(self):
+        if self.grid_supply < 0:
+            return self.solar_production
+        else:
+            return self.solar_production - self.grid_supply
+
+
+    @property
+    def self_sufficiency(self):
+        hc = self.home_consumption
+        if hc > 0:
+            return self.solar_self_consumption / hc
+        else:
+            return 0
+        
+    @property 
+    def solar_energy(self):
+        """Solar energy in kWh"""
+        return self._solar_energy.value / (3600 * 1000)
+    
+    @property
+    def consumed_energy(self):
+        """Consumed energy in kWh"""
+        return self._consumed_energy.value / (3600 * 1000)
+        
+
+
+    def update_state(self, topic: str, message: str):
+        current_time = datetime.now()
+        time_stamp = current_time.timestamp()
+        if topic == self.solar_mqtt_topic + "/state":
+            self.solar_production = float(message)    
+            self._solar_energy.add_measurement(self.solar_self_consumption, time_stamp)
+            self._consumed_energy.add_measurement(self.home_consumption, time_stamp)            
+        elif topic == self.grid_supply_mqtt_topic + "/state":
+            self.grid_supply = float(message)    
+            print("Consumption: ", end='')
+            self._solar_energy.add_measurement(self.solar_self_consumption, time_stamp)
+            self._consumed_energy.add_measurement(self.home_consumption, time_stamp)                 
+        for device in self.devices:
+            device.update_state(topic, message)
+
+
+    def mqtt_topics(self):
+        result = [self.solar_mqtt_topic, self.grid_supply_mqtt_topic]
+        for device in self.devices:
+            result.append(device.mqtt_topic)        
+        return result
+
+
