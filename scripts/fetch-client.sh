d0632984648e3849dfe4ca0ed4d1ac8eb6c11520
Paul Frank <pail@gmx.net>
2023-05-15 19:29:32 +0000

    fix script

--- scripts/fetch-client.sh
+++ scripts/fetch-client.sh
@@ -1,4 +1,4 @@
-rm client/* -rf
+rm client -rf
 cd client
 wget https://github.com/pail23/energy-assistant-frontend/releases/download/0.0.1/energy-assistant-frontend.zip
 unzip energy-assistant-frontend.zip
