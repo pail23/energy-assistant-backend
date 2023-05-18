ea8027b9ce523a50a39b92ce240d38742240d244
Paul Frank <pail@gmx.net>
2023-05-18 05:44:10 +0000

    fix

--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -1,1 +1,1 @@
-docker run -p 8080:80 --rm  -v "$(pwd)/config.yaml:/config/config.yaml" energy-assistant:latest
+docker run -p 8080:80 --rm  -v "$(pwd)/energy_assistant.yaml:/config/energy_assistant.yaml" energy-assistant:latest
