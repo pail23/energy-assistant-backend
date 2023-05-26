f05c4b9109f85eca207beadd1ae1db8912147876
Paul Frank <pail@gmx.net>
2023-05-26 19:39:21 +0000

    first test

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,1 @@
-"""Application."""
+"""The applicaton."""
--- tests/test_devices.py
+++ tests/test_devices.py
@@ -2,9 +2,9 @@
 from app.devices import EnergyIntegrator
 
 
-async def test_integrator() -> None:
+def test_integrator() -> None:
     """Test the energy integrator."""
     integrator = EnergyIntegrator()
-    integrator.add_measurement(10, 10)
-    integrator.add_measurement(10, 10)
+    integrator.add_measurement(10, 0)
+    integrator.add_measurement(20, 0.1)
     assert integrator.consumed_solar_energy == 11
