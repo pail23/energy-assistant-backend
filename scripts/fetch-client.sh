d0b574c6e14bfda3cfc6a2b2b54db30005706a4d
Paul Frank <pail@gmx.net>
2023-09-23 16:06:51 +0000

    bump ui to 0.0.38

--- scripts/fetch-client.sh
+++ scripts/fetch-client.sh
@@ -1,7 +1,7 @@
 rm client -rf
 mkdir client
 cd client
-wget https://github.com/pail23/energy-assistant-frontend/releases/download/0.0.37/energy-assistant-frontend.zip
+wget https://github.com/pail23/energy-assistant-frontend/releases/download/0.0.38/energy-assistant-frontend.zip
 unzip energy-assistant-frontend.zip
 rm energy-assistant-frontend.zip
 cd ..
