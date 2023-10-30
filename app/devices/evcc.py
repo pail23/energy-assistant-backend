af2e0730a84d94e32bd0dc5f76ff5dbbd6f36318
Paul Frank <pail@gmx.net>
2023-10-30 06:35:40 +0000

    change from chargedEnergy to sessionEnergy in EVCC

--- app/devices/evcc.py
+++ app/devices/evcc.py
@@ -65,7 +65,7 @@ class EvccDevice(DeviceWithState):
             self.get_device_topic_id("chargeTotalImport")
         )
         if self.consumed_energy == 0:
-            state = state_repository.get_state(self.get_device_topic_id("chargedEnergy"))
+            state = state_repository.get_state(self.get_device_topic_id("sessionEnergy"))
             if state is not None:
                 self._consumed_energy = self._utility_meter.update_energy_state(state)
         self._consumed_solar_energy.add_measurement(self.consumed_energy, self_sufficiency)
