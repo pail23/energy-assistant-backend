a400d9da1d590497a53e4907cadd1f40418e5315
Paul Frank <pail@gmx.net>
2024-07-20 18:29:37 +0000

    implement put config value

--- energy_assistant/api/config/use_cases.py
+++ energy_assistant/api/config/use_cases.py
@@ -32,3 +32,18 @@ class ReadDeviceConfiguration:
     async def execute(self, config: EnergyAssistantConfig, device_id: uuid.UUID) -> ConfigModel:
         """Execute the read configuration use case."""
         return ConfigModel.model_validate({"config": config.energy_assistant_config.devices.get_device_config(device_id)})
+
+
+class WriteDeviceConfiguration:
+    """Write the configuration values on a device."""
+
+    def __init__(self, session: AsyncSession) -> None:
+        """Create a write configuration use case."""
+        self.async_session = session
+
+    async def execute(self, config: EnergyAssistantConfig, data: dict, device_id: uuid.UUID) -> ConfigModel:
+        """Execute the write configuration use case."""
+        for (key, value) in data.items():
+            config.energy_assistant_config.devices.set(device_id, key, value)
+
+        return ConfigModel.model_validate({"config": config.energy_assistant_config.devices.get_device_config(device_id)})
--- energy_assistant/api/config/views.py
+++ energy_assistant/api/config/views.py
@@ -6,7 +6,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, Path, Request
 
 from .schema import ConfigModel, ReadConfigResponse, ReadDeviceConfigResponse
-from .use_cases import ReadConfiguration, ReadDeviceConfiguration
+from .use_cases import ReadConfiguration, ReadDeviceConfiguration, WriteDeviceConfiguration
 
 router = APIRouter(prefix="/config")
 
@@ -37,3 +37,20 @@ async def read(
     if energy_assistant is None:
         raise HTTPException(status_code=500)
     return await use_case.execute(energy_assistant.config, device_id)
+
+
+@router.put(
+    "/device/{device_id}",
+    response_model=ReadDeviceConfigResponse,
+)
+async def write(
+    request: Request,
+    data: dict,
+    device_id: Annotated[uuid.UUID, Path(..., description="")],
+    use_case: Annotated[WriteDeviceConfiguration, Depends(WriteDeviceConfiguration)],
+) -> ConfigModel:
+    """REST end point for read a device configuration."""
+    energy_assistant = request.app.energy_assistant if hasattr(request.app, "energy_assistant") else None
+    if energy_assistant is None:
+        raise HTTPException(status_code=500)
+    return await use_case.execute(energy_assistant.config, data, device_id)
