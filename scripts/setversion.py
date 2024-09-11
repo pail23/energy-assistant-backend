6cd7c035de214b38f101ac3c028f0722eff38f43
Paul Frank <pail@gmx.net>
2024-09-11 12:59:43 +0000

    fix

--- scripts/setversion.py
+++ scripts/setversion.py
@@ -1,14 +1,17 @@
+"""Set the project version in the pyproject.toml file."""
+
 import sys
+from pathlib import Path
 
 import tomli
 import tomli_w
 
-with open("pyproject.toml", "rb") as f:
+with Path("pyproject.toml").open(mode="rb") as f:
     pyproject = tomli.load(f)
 
 print(f"set version to {sys.argv[1]}")
 
 pyproject["project"]["version"] = sys.argv[1]
 
-with open("pyproject.toml", "wb") as f:
+with Path("pyproject.toml").open("wb") as f:
     tomli_w.dump(pyproject, f)
